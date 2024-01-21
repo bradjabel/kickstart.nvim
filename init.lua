8f5f98494ca98efec1d21ef9fc168a372b92c448
unknown <brad.j.abel@gmail.com>
2024-01-21 08:40:24 -0700

    disable markdown treesitter highlight

--- init.lua
+++ init.lua
@@ -445,7 +445,7 @@ vim.defer_fn(function()
     ignore_install = {},
     -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
     modules = {},
-    highlight = { enable = true },
+    highlight = { enable = true, disable = { "markdown" } },
     indent = { enable = true },
     incremental_selection = {
       enable = true,
