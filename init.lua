f2f7150d29060dedb287d13f6ef39131fa51c5df
unknown <brad.j.abel@gmail.com>
2024-02-20 20:02:30 -0700

    backup

--- init.lua
+++ init.lua
@@ -79,7 +79,8 @@ require('lazy').setup({
     "ggandor/leap.nvim",
     dependencies = { "tpope/vim-repeat" },
     config = function()
-      require("leap").create_default_mappings()
+      -- -help leap-custom-mappings to find more
+      vim.keymap.set({ 'n' }, 's', '<Plug>(leap-forward)');
       vim.keymap.set({ 'n' }, '\\', '<Plug>(leap-backward)');
     end
   },
