c1a7ddce20a9e1f95ede2daed6e988db115c5133
unknown <brad.j.abel@gmail.com>
2025-01-05 14:12:52 -0700

    fix lazy tables

--- init.lua
+++ init.lua
@@ -777,12 +777,12 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { { 'prettierd', 'prettier', stop_after_first = true } },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
         json = { 'fixjson' },
-        typescript = { { 'prettierd', 'prettier', stop_after_first = true } },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
         sql = { 'sqlfmt' },
         html = { 'prettierd' },
-        svelte = { { 'prettierd', 'prettier', stop_after_first = true } },
+        svelte = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
